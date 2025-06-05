6fe0c3cc1531ff85691db07851a6f5e75eb0a2f2
harrison-anth <harrison.anthony@wsu.edu>
2025-06-05 19:55:17 +0100

    updating gitignore and primary submission scripts

--- scripts/annotate_bamog.R
+++ scripts/annotate_bamog.R
@@ -1,6 +1,6 @@
-#annotate filtered h5 files
-#load libraries
-library(numbat)
+#### Annotate filtered h5 files ####
+
+# Load libraries
 library(Seurat)
 library(patchwork)
 library(tidyverse)
@@ -8,22 +8,21 @@ library(scAnnotatR)
 library(data.table)
 library(R.utils)
 library(data.table)
-library(maftools)
-library(foreach)
-library(parallel)
 
-#add command line arguments
+# Receive arguments from command line
 argus <- (commandArgs(asValues=TRUE, excludeReserved=TRUE)[-1])
 sample_name <- as.character(argus[1])
+key <- fread(as.character(argus[2]))
+msi_cutoff <- as.numeric(argus[3])
 
-#set seed
-set.seed(seed = 152727)
+indv_key <- filter(key, filename == sample_name)
 
-# functions to add metadata to seurat object
+# Functions to add metadata to seurat object
 add_sensor_rna <- function(s_obj,sample_name,threshold){
 sensor_rna_results <- fread(paste0('../sensor_rna_results/',sample_name,'.txt'))
-sensor_rna_results$msi_status[sensor_rna_results$`probability_of_MSI-H` >= .75 ] <- 'MSI-H'
-sensor_rna_results$msi_status[sensor_rna_results$`probability_of_MSI-H` < .75 ] <- 'MSS'
+sensor_rna_results$msi_status[sensor_rna_results$`probability_of_MSI-H` >= msi_cutoff ] <- 'MSI-H'
+sensor_rna_results$msi_status[sensor_rna_results$`probability_of_MSI-H` < msi_cutoff ] <- 'MSS'
+
 temp <- t(sensor_rna_results)
 colnames(temp) <- temp[1,]
 temp <- temp[2:3,]
@@ -36,29 +35,18 @@ new_sample <- readRDS(paste0('../filtered_h5/',sample_name,'.rds'))
 
 new_sample <- add_sensor_rna(new_sample,sample_name)
 
-#add big picture things
+# Add scAOMIC and other metadata
 
 new_sample[['sample_name']] <- sample_name
 
-new_key <- fread('../manifests/combined_key.tsv')
-sample_info <- filter(new_key, filename == sample_name)
-
-new_sample[['tissue']] <- sample_info$site
-
-#quickly add atomic cell labels
+new_sample[['tissue']] <- indv_key$site
 atomic_cells <- readRDS(paste0('../atomic/',sample_name,'.rds'))
 
 new_sample$scATOMIC_pred <- atomic_cells$scATOMIC_pred
-
 new_sample$classification_confidence <- atomic_cells$classification_confidence
-
-
 if('pan_cancer_cluster' %in% colnames(atomic_cells@meta.data)){
 new_sample$pan_cancer_cluster <- atomic_cells$pan_cancer_cluster
 }
-
-
-
 
 saveRDS(new_sample, paste0('../annotated_h5/',sample_name,'.rds'))
 
---
+++ scripts/plot_all_bamog.R
@@ -0,0 +1,43 @@
+#### Create plots for each sample prior to integration ####
+
+# Load libraries
+library(Seurat)
+library(gridExtra)
+library(patchwork)
+library(tidyverse)
+library(scAnnotatR)
+library(data.table)
+library(cowplot)
+library(Nebulosa)
+library(ggpubr)
+library(R.utils)
+set.seed(seed = 152727)
+
+# Receive arguments from command line
+argus <- (commandArgs(asValues=TRUE, excludeReserved=TRUE)[-1])
+sample_name <- as.character(argus[1])
+
+plotter <- function(sample_name){
+
+s_obj <- readRDS(paste0('../annotated_h5/',sample_name,'.rds'))
+atomic_cells <- readRDS(paste0('../atomic/',sample_name,'.rds'))
+
+s_obj$scATOMIC_pred <- atomic_cells$scATOMIC_pred
+s_obj$pan_cancer_cluster <- atomic_cells$pan_cancer_cluster
+s_obj$sensor_rna_prob <- as.numeric(s_obj$sensor_rna_prob)
+
+plots <- ggarrange( 
+DimPlot(s_obj)+ggtitle(paste0('Clusters')),
+DimPlot(s_obj,group.by='pan_cancer_cluster',cols=c('red','blue','grey'))+ggtitle(paste0('ATOMIC')),
+DimPlot(s_obj,group.by='sensor_rna_status',cols=c('red','blue'))+ggtitle(paste0('sensor_rna')),
+FeaturePlot(s_obj,features=c('sensor_rna_prob'),cols=c('blue','red')) +ggtitle('sensor_rna probs')),
+nrow=3,nrow=2)
+
+plots_final <- annotate_figure(plots,top=text_grob(paste0('Sample: ', sample_info$filename),color='black',face='bold',size=20)))
+
+ggsave(paste0('../reports/',sample,'_sample_report.pdf'),plots_final)
+
+}
+
+
+plotter(sample_name)
