e1d4dc2b38214d6647957b03dff2c0ff73892628
harrison-anth <harrison.anthony@wsu.edu>
2025-06-06 19:15:06 +0100

    removing unnecessary dir and adding parallelized reporter script

--- scripts/patient_report_generator.R
+++ scripts/patient_report_generator.R
@@ -1,10 +1,15 @@
+#### Create nice markdown patient reports ####
+
+# Load libraries
 library(R.utils)
 library(rmarkdown)
 
+
+# Receive arguments from the command line
 argus <- (commandArgs(asValues=TRUE, excludeReserved=TRUE)[-1])
 sample_name <- as.character(argus[1])
 
 
-#run the markdown
+# Call Rmarkdown
 
-rmarkdown::render('../markdown_files/parallelized_reporter3.rmd',output_file=paste0('../patient_reports/',sample_name,'.html'),params=list(sample_id=sample_name))
+rmarkdown::render('parallelized_reporter.rmd',output_file=paste0('../reports/',sample_name,'.html'),params=list(sample_id=sample_name))
